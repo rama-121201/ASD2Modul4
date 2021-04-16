4ab847d8efc6cfaff8018df3bf40f4635efd0f28
rama-121201 <72060246+rama-121201@users.noreply.github.com>
2021-04-16 17:20:54 +0700

    Add files via upload

---
+++ src/main/java/Tugas4/nomer5.java
@@ -0,0 +1,50 @@
+package Tugas4;
+public class nomer5 {
+    public static void main(String[] args) {
+        double A [] = {25,7,9,13,3};
+        System.out.println("Data yang belum diurutkan : ");
+        nomer5.print(A);
+        nomer5.quicksort(A,0,A.length-1);
+        System.out.println("Data yang sudah diurutkan : ");
+        nomer5.print(A);
+        System.out.println("Rama Anandya Putra");
+    }
+    public static int partition(double [] A,int B,int C ){
+        int x,y;
+        double pivot;
+        pivot = A[B];
+        x = B-1;
+        y = C + 1;
+        for(;;){
+            do{
+                x++;
+            }while(A[x]< pivot);
+            do{
+                y--;
+            }while(A[y]> pivot);
+            
+            if(x<y){
+                double temp = A[x];
+                A[x] = A[y];
+                A[y] = temp;
+            }else{
+                return y;
+            }
+        }
+    }
+    public static void quicksort(double[] A,int B,int C){
+        int D;
+        if(B<C){
+            D =partition (A,B,C);
+            quicksort(A,B,D);
+            quicksort(A,D+1,C);
+        }
+    }
+    public static void print(double [] data){
+        for (int i = 0; i < data.length; i++) {
+            System.out.print(data[i]+" ");
+            
+        }
+        System.out.println();
+    }
+}
